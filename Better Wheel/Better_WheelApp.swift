12b746275fbe5789d0461601dff67769250f2d27
masooddalman <m.dalman68@gmail.com>
2026-02-01 15:12:05 +0300

    empty project

---
+++ Better Wheel/Better_WheelApp.swift
@@ -0,0 +1,17 @@
+//
+//  Better_WheelApp.swift
+//  Better Wheel
+//
+//  Created by MasoodDalman on 1.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct Better_WheelApp: App {
+    var body: some Scene {
+//        WindowGroup {
+//            ContentView()
+//        }
+    }
+}
---
+++ Better Wheel/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Better Wheel
+//
+//  Created by MasoodDalman on 1.02.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
